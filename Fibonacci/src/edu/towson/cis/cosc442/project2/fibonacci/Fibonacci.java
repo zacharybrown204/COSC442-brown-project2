fd76dce5ef98c7b334cd3ce4a3e16d60474935b4
Zachary Brown <zacharybrown204@yahoo.com>
2016-02-18 20:09:39 -0500

    Fixed Fibonacci so that the 0th term is 0.

--- Fibonacci/src/edu/towson/cis/cosc442/project2/fibonacci/Fibonacci.java
+++ Fibonacci/src/edu/towson/cis/cosc442/project2/fibonacci/Fibonacci.java
@@ -14,7 +14,7 @@ public class Fibonacci {
 	 */
 	public int fibonacci(int n) {
 		switch (n) {
-			case 0: return 1;
+			case 0: return 0;
 			case 1: return 1;
 			default: return (fibonacci(n - 1) + fibonacci(n - 2));
 		}
