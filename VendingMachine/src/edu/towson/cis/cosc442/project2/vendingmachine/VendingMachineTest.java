17ef0b340d283c265288badac8a2ecd7a383c57f
Zachary Brown <zacharybrown204@yahoo.com>
2016-02-18 21:23:41 -0500

    Created test classes. Item tests are complete.

---
+++ VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,49 @@
+package edu.towson.cis.cosc442.project2.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VendingMachineTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertMoney() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakePurchase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReturnChange() {
+		fail("Not yet implemented");
+	}
+
+}
