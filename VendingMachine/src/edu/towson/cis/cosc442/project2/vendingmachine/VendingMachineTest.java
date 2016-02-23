27271b95e04f8b6e981850034af0bbdf6d757a52
Zachary Brown <zacharybrown204@yahoo.com>
2016-02-23 01:55:14 -0500

    Completed Tests for the Vending Machine class.

--- VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
+++ VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
@@ -8,42 +8,95 @@ import org.junit.Test;
 
 public class VendingMachineTest {
 
+	VendingMachine vend1;
+	VendingMachineItem item1, item2;
+	static final double cost1 = 10;
+	static final double cost2 = 20;
+
 	@Before
 	public void setUp() throws Exception {
+		vend1 = new VendingMachine();
+
+		item1 = new VendingMachineItem("Test1", cost1);
+		item2 = new VendingMachineItem("Test2", cost2);
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		vend1 = null;
+		item1 = null;
+		item2 = null;
 	}
 
 	@Test
 	public void testAddItem() {
-		fail("Not yet implemented");
+		vend1.addItem(item1, "A");
+
+		try {
+			vend1.addItem(item2, "A");
+			fail("Expected exception");
+		} catch (VendingMachineException e) {
+
+		}
+
 	}
 
 	@Test
 	public void testRemoveItem() {
-		fail("Not yet implemented");
+		vend1.addItem(item1, "A");
+		assertEquals(item1, vend1.removeItem("A"));
+
+		try {
+			vend1.removeItem("B");
+			fail("Expected Exception");
+		} catch (VendingMachineException e) {
+
+		}
+
 	}
 
 	@Test
 	public void testInsertMoney() {
-		fail("Not yet implemented");
+		vend1.insertMoney(20);
+
+		try {
+			vend1.insertMoney(-20);
+			fail("Expected Exception");
+		} catch (VendingMachineException e) {
+
+		}
 	}
 
 	@Test
 	public void testGetBalance() {
-		fail("Not yet implemented");
+		assertEquals(0, vend1.getBalance(), .01);
+		
+		vend1.insertMoney(20);
+		assertEquals(20,vend1.getBalance(), .01);
 	}
 
 	@Test
 	public void testMakePurchase() {
-		fail("Not yet implemented");
+		vend1.insertMoney(20);
+		vend1.addItem(item1, "A");
+		assertEquals(true,vend1.makePurchase("A"));
+		assertEquals(10,vend1.getBalance(), .01);
+		
+		assertEquals(false,vend1.makePurchase("B"));
+		vend1.addItem(item2, "B");
+		assertEquals(false,vend1.makePurchase("B"));
 	}
 
 	@Test
 	public void testReturnChange() {
-		fail("Not yet implemented");
+		vend1.insertMoney(20);
+		assertEquals(20,vend1.returnChange(), .01);
+		
+		vend1.addItem(item1, "A");
+		vend1.insertMoney(20);
+		vend1.makePurchase("A");
+		assertEquals(10, vend1.returnChange(), .01);
 	}
 
 }
